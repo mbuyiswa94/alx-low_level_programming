1f31e8b3c041f1742ba0ece89fe6a986f3816140
mbuyiswa94 <nkomo.mbuyiswa@gmail.com>
2023-08-04 18:34:35 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	if (argc != 3)
+
+	printf("Error\n");
+	return (0);
+}
+
